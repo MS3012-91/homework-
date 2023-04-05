ff0face477668b2a75f84bbe6d2000bca0deb65f
MS 3012-91 <mariasheinyk@gmail.com>
2023-04-05 20:59:34 +0200

    done!

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const enterTodo = document.querySelector ('input[name = "addTodo"]');
 const newElements = document.querySelector ('.newElements');
 
 
-let total = 0;
+
 btnadd.addEventListener ('click', createNewItem); 
-let count =localStorage.length;
 
+  let keys = Object.keys (localStorage);
+  count = Math.max.apply(null, keys)+1;
 
-
-function createNewItem () {
+    function createNewItem () {
     let item = document.createElement ('li');
     item.className = "newItem";   
     let content = enterTodo.value;
@@ -22,13 +22,16 @@ function createNewItem () {
     }
     else {
      newElements.appendChild (item);
+
      localStorage.setItem (count, content);
      let txt = document.createTextNode (localStorage.getItem(count));
      item.setAttribute('count', count);
+    
+
      count++;
      enterTodo.value = '';
      item.appendChild (txt);
-     total++;
+     changeText()
  }
     let closeButton = document.createElement ('button');
     closeButton.className = 'closeButton';
@@ -37,79 +40,56 @@ function createNewItem () {
     closeButton.onclick = function () {
     closeButton.parentElement.className = 'deleted';
     let a = closeButton.parentElement.getAttribute('count');
-    count=newElements.lastChild.getAttribute;
-    localStorage.removeItem(a);
-    total--;
+    localStorage.removeItem(a); 
+    changeText()
+    
+}
 }
 
-}
 
-
-
-let i=0;
 window.addEventListener ('load', () => {
     if (localStorage.length !=0) {
         let i=0;
 
-
-        for (i=0; i< localStorage.length; i++) {
-            if (!localStorage.key(i)) {
+            for (i=0; i< 10000; i++) {
+            if (localStorage[i]===undefined) {
             continue
             }
             
             else {
             let count= localStorage.key(i);
-
             let item = document.createElement ('li');
             item.className = "newItem";
-             newElements.appendChild (item);
-             item.setAttribute('count', count);
-             let txt = document.createTextNode (localStorage.getItem(count));
-            //count++;
+            item.setAttribute('count', count);
+            newElements.appendChild (item);
+
+            let content = localStorage.getItem(JSON.stringify(i))
+            let txt = document.createTextNode (content);
+            item.setAttribute ('i', i);
             item.appendChild (txt);
-        
+            changeText()
             let closeButton = document.createElement ('button');
             closeButton.className = 'closeButton';
             closeButton.innerText = "X"
             item.appendChild (closeButton);
             closeButton.onclick = function () {
             closeButton.parentElement.className = 'deleted';
-            let a = closeButton.parentElement.getAttribute('count');
-            count=newElements.lastChild.getAttribute;
+            let a = closeButton.parentElement.getAttribute('i');
             localStorage.removeItem(a); 
-            // for (a; a<localStorage.length; ++a) {
-            //     item = document.querySelector ('.newItem');
-            //     item.setAttribute('count', a);
-            // }
-            
-            //count--;
-            }
-            
-           } }
-        total++
-    }})
+            changeText()
+            }           } }      } } )
         
-
-    
+ 
 clearAll.onclick = function () {newElements.replaceChildren();
 localStorage.clear();
 count=0;
+changeText()
 }
 
-
-
-enterTodo.addEventListener ('keypress', (pressed) => {
-    const keyEnter = 13;
-    if (pressed.which == keyEnter) {
-        createNewItem ()
-    }
-});
-
+function changeText () {
 let pt = document.querySelector ('span'); 
-pt.innerText = localStorage.length; //меняется только при обновлении стр.
-
-
-
+pt.innerText = localStorage.length; 
+}
 
 
 
